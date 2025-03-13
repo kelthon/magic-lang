63780c2cdbd239257bbb5e75f615ccf16726006d
Kelthon <kelthonbalbino@gmail.com>
2025-03-13 20:20:06 -0300

    feat(langdef): add langdef.h for language definition tokens
    Added langdef.h file to store common language definitions such as operators like '=='.

---
+++ include/langdef.h
@@ -0,0 +1,75 @@
+#pragma once
+
+// Character definitions
+#define ZERO_CHAR '0'
+#define SPACE_CHAR ' '
+#define NEWLINE_CHAR '\n'
+#define TABULATION_CHAR '\t'
+#define COMMA_CHAR ','
+#define COLON_CHAR ':'
+#define QUOTE_CHAR '\''
+#define DOUBLE_QUOTE_CHAR '"'
+#define DECIMAL_POINT_CHAR '.'
+
+// Common delimiters
+#define PARENTHESIS_OPEN '('
+#define PARENTHESIS_CLOSE ')'
+#define BRACKET_OPEN '['
+#define BRACKET_CLOSE ']'
+
+// Block delimiters
+#define BLOCK_START '{'
+#define BLOCK_END '}'
+
+// Comment delimiters
+#define SINGLE_LINE_COMMENT "//"
+#define MULTILINE_COMMENT_START "/*"
+#define MULTILINE_COMMENT_END "*/"
+
+// Statement terminators
+#define STATEMENT_TERMINATOR ';'
+
+// Member access operator
+#define MEMBER_ACCESS_OPERATOR '.'
+
+// Arithmetic operators
+#define ADDITION_OPERATOR '+'
+#define SUBTRACTION_OPERATOR '-'
+#define MULTIPLICATION_OPERATOR '*'
+#define DIVISION_OPERATOR '/'
+#define MODULO_OPERATOR '%'
+
+// Bitwise operators.
+#define BITWISE_AND_OPERATOR '&'
+#define BITWISE_OR_OPERATOR '|' 
+#define BITWISE_XOR_OPERATOR '^'
+#define BITWISE_NOT_OPERATOR '~'
+
+// Bitwise shift operators
+#define BITWISE_SHIFT_LEFT "<<"
+#define BITWISE_SHIFT_RIGHT ">>"
+
+// Assignment operators
+#define SIMPLE_ASSIGN_OPERATOR '='
+#define ADD_ASSIGN_OPERATOR "+="
+#define SUB_ASSIGN_OPERATOR "-="
+#define MUL_ASSIGN_OPERATOR "*="
+#define DIV_ASSIGN_OPERATOR "/="
+#define MOD_ASSIGN_OPERATOR "%="
+#define LEFT_SHIFT_ASSIGN_OPERATOR "<<="
+#define RIGHT_SHIFT_ASSIGN_OPERATOR ">>="
+
+// Unary operators
+#define INCREMENT_OPERATOR "++"
+#define DECREMENT_OPERATOR "--"
+
+// Comparison operators
+#define EQUALITY_OPERATOR "=="
+#define INEQUALITY_OPERATOR "!="
+#define GREATER_THAN_OPERATOR '>'
+#define LESS_THAN_OPERATOR '<'
+#define GREATER_OR_EQUAL_OPERATOR ">="
+#define LESS_OR_EQUAL_OPERATOR "<="
+#define AND_OPERATOR "&&"
+#define OR_OPERATOR "||" 
+#define NOT_OPERATOR '!'
