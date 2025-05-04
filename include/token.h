57e3eac650ec5a7b0ef55a61ca6b049a81edf257
Kelthon <kelthonbalbino@gmail.com>
2025-05-04 15:27:29 -0300

    feat: updated token index, create isTypeDeclaration function

--- include/token.h
+++ include/token.h
@@ -4,28 +4,65 @@
 
 using std::string;
 
-enum Tag {
-  TYPE = 256,
-  KEYWORD,
+enum TokenType {
+  TK_EOF = -1,
+
+  // Types
+  TK_TYPE_INT = -2,
+  TK_TYPE_BOOL = -3,
+  TK_TYPE_CHAR = -4,
+  TK_TYPE_FLOAT = -5,
+  TK_TYPE_DOUBLE = -6,
+  TK_TYPE_STRING = -7,
+  TK_TYPE_CLASS = -8,
+
+  // Identifiers
+  TK_IDENTIFIER = -9,
+
+  // Keywords
+  TK_IF = -10,
+  TK_ELSE = -11,
+  TK_WHILE = -12,
+  TK_FOR = -13,
+  TK_RETURN = -14,
 
   // Literals
-  LITERAL_BOOLEAN,
-  LITERAL_CHAR,
-  LITERAL_FLOAT,
-  LITERAL_INTEGER,
+  TK_LITERAL_INT = -15,
+  TK_LITERAL_BOOL = -16,
+  TK_LITERAL_CHAR = -17,
+  TK_LITERAL_FLOAT = -18,
+  TK_LITERAL_DOUBLE = -19,
+  TK_LITERAL_STRING = -20,
+  TK_LITERAL_CLASS = -21,
+  TK_LITERAL_NULL = -22,
 
-  // Operator
-  OPERATOR,
+  // Operators
+  TK_OPERATOR = -23,
+
+  // Access modifiers
+  TK_ACCESS_MODIFIER = -24,
+
+  // Modifiers
+  TK_MODIFIER = -25,
+
+  // Object Instance
+  TK_OBJ_INSTANCE = -26,
 };
 
 class Token {
- public:
-  Token() : tag(0) {}
-  Token(char character) : tag(int(character)), lexeme({character}) {}
-  Token(int tag, string lexeme) : tag(tag), lexeme(lexeme) {}
+  public:
+  Token() = default;
+  Token(char character) : type(int(character)), lexeme({character}) {}
+  Token(int type, string lexeme) : type(type), lexeme(lexeme) {}
+  
+  int getType() { return type; }
+  string getLexeme() { return lexeme; }
+  
   virtual string toString();
-
- private:
-  uint tag;
+  
+  private:
+  int type;
   string lexeme;
 };
+
+bool isTypeDeclaration(Token* token);
--- src/token.cpp
+++ src/token.cpp
@@ -10,3 +10,18 @@ string Token::toString() {
 
   return buffer.str();
 }
+
+bool isTypeDeclaration(Token *token) {
+  switch (token->getType()) {
+    case TK_TYPE_INT:
+    case TK_TYPE_BOOL:
+    case TK_TYPE_CHAR:
+    case TK_TYPE_FLOAT:
+    case TK_TYPE_DOUBLE:
+    case TK_TYPE_STRING:
+    return true;
+
+    default:
+      return false;
+  }
+}
