ce5a642fdd9a10d4da8133a9331f5e75d500e1ef
Kelthon <kelthonbalbino@gmail.com>
2025-05-23 12:37:08 -0300

    feat: added new ast nodes, renamed ClassDeclaration node
    added IntegerLiteral node, added FloatLiteral node, added Char
    Literal node, added Attribute node, renamed ClassDeclaration node
    to Declaration node

--- include/ast.h
+++ include/ast.h
@@ -4,9 +4,12 @@
 #include <memory>
 #include <vector>
 
+using std::stof;
+using std::stoi;
+using std::move;
 using std::string;
-using std::unique_ptr;
 using std::vector;
+using std::unique_ptr;
 
 namespace ast {
 // Abstract Classes
@@ -17,7 +20,41 @@ class Node {
   virtual ~Node() = default;
 };
 
-class Identifier : public Node {
+class Value : public Node {
+ public:
+  Value() = default;
+  virtual string codegen() = 0;
+  virtual ~Value() = default;
+};
+
+class IntegerLiteral : public Value {
+ public:
+  IntegerLiteral(string value) : value(stoi(value)) {}
+  string codegen() override;
+
+ private:
+  int value;
+};
+
+class FloatLiteral : public Value {
+ public:
+  FloatLiteral(string value) : value(stof(value)) {}
+  string codegen() override;
+
+ private:
+  float value;
+};
+
+class CharLiteral : public Value {
+ public:
+  CharLiteral(string value) : value(value.c_str()[0]) {}
+  string codegen() override;
+
+ private:
+  char value;
+};
+
+class Identifier : public Value {
  public:
   Identifier(string name) : name(name) {};
   string codegen() override;
@@ -26,24 +63,41 @@ class Identifier : public Node {
   string name;
 };
 
-class ClassDeclaration : public Node {
+class Attribute : public Node {
  public:
-  ClassDeclaration(unique_ptr<Identifier> className)
-      : className(move(className)) {}
+  Attribute(string typeName, unique_ptr<Identifier> attributeName,
+            unique_ptr<Value> value)
+      : typeName(typeName),
+        attributeName(move(attributeName)),
+        value(move(value)) {};
+  string codegen() override;
+
+ private:
+  string typeName;
+  unique_ptr<Identifier> attributeName;
+  unique_ptr<Value> value;
+};
+
+class Declaration : public Node {
+ public:
+  Declaration(unique_ptr<Identifier> className,
+              vector<unique_ptr<Attribute>> classAttributes)
+      : className(move(className)), classAttributes(move(classAttributes)) {}
   string codegen() override;
 
  private:
   unique_ptr<Identifier> className;
+  vector<unique_ptr<Attribute>> classAttributes;
 };
 
 class Program : public Node {
  public:
-  Program(unique_ptr<ClassDeclaration> classDeclaration)
+  Program(unique_ptr<Declaration> classDeclaration)
       : classDeclaration(move(classDeclaration)) {}
   string codegen() override;
 
  private:
-  unique_ptr<ClassDeclaration> classDeclaration;
+  unique_ptr<Declaration> classDeclaration;
 };
 
 }  // namespace ast
--- src/ast.cpp
+++ src/ast.cpp
@@ -8,11 +8,41 @@ using std::stringstream;
 using std::to_string;
 using namespace ast;
 
+string IntegerLiteral::codegen() { return to_string(value); }
+
+string FloatLiteral::codegen() { return to_string(value); }
+
+string CharLiteral::codegen() { return to_string(value); }
+
 string Identifier::codegen() { return name; }
 
-string ClassDeclaration::codegen() {
-  string cname = className->codegen() + "_class";
-  return "typedef struct " + cname + " {} " + cname + ";";
+string Attribute::codegen() {
+  stringstream code;
+
+  code << typeName + " " + attributeName->codegen();
+
+  if (value != nullptr) {
+    code << "=" + value->codegen();
+  }
+
+  code << ";";
+
+  return code.str();
+}
+
+string Declaration::codegen() {
+  stringstream code;
+  string classId = "_class_" + className->codegen();
+
+  code << "typedef struct {";
+
+  for (const unique_ptr<Attribute>&attribute : classAttributes) {
+    code << attribute->codegen();
+  }
+
+  code << "}" + classId + ";";
+
+  return code.str();
 }
 
 string Program::codegen() { return classDeclaration->codegen(); }
